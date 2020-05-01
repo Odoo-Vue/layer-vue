4e892f01d4602e39425649b189bb22b54a8ae6be
dga <dga@open-net.ch>
2020-05-01 14:39:28 +0200

    trying to do jsonrpc request to product.product

--- src/main.js
+++ src/main.js
@@ -2,8 +2,32 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios"
+// import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
+
+const response = axios.post('/jsonrpc', {
+  jsonrpc: "2.0",
+  model: "product.product",
+  method: "search",
+  args: [],
+  // params: {  //args
+  //   domain: []
+  // },
+}, {
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+  },
+});
+
+    //   self._rpc({
+    //     model: 'ir.model.data',
+    //     method: 'search',
+    //     args: [domain],
+    // })
+console.log(response.data.result.address)
 
 new Vue({
   router,
