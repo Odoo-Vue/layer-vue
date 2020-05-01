9e6a18b1d42ce183fa928fa3a3354fc3a9ed322d
dga <dga@open-net.ch>
2020-05-01 13:42:57 +0200

    can render vuejs

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Odoo Vuejs',
+    'version': '12.0.1.0.0',
+    'category': 'Others',
+    'depends': [
+        'base',
+    ],
+    'author': 'Gallay David',
+    'license': 'AGPL-3',
+    'description': """Vuejs frontend in odoo snippet""",
+    'data': [
+    ],
+ }
